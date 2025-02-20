c528078d0ea07328c894dda7ef89589f5cbc5a70
Jmspter <jamesideias@gmail.com>
2025-02-20 10:44:19 -0300

    Salvando alterações antes do pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,16 @@ class PagesController < ApplicationController
   end
 
   def home_on
+    # Lista de nomes para escolha aleatória
+    @names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+    
+    # Escolhe um nome aleatório
+    @random_name = @names.sample
+    
+    # Se o usuário enviar um novo nome, atualizamos a variável
+    if params[:new_name].present?
+      @random_name = params[:new_name]
+    end
     @courses = Course.all
     @greeting = case Time.current.hour
     when 5..11
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   protected
-
   def after_sign_in_path_for(resource)
     if resource.admin?
       admin_dashboard_path
