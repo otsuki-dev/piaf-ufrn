abac030c2dd6ee404a7ccc5108e9531ab87c48e4
Jmspter <jamesideias@gmail.com>
2025-03-05 07:53:54 -0300

    envio de email pelo admin

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,34 +1,39 @@
 class EmailsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :require_admin
-  
-    def create
-      recipients = case params[:recipient_type]
-                  when 'all'
-                    User.all
-                  when 'active_course'
-                    Course.find(params[:course_id]).enrollments.map(&:user)
-                  when 'finished_course'
-                    Course.find(params[:course_id]).enrollments.map(&:user)
-                  else
-                    []
-                  end
-  
-      if recipients.any?
-        recipients.each do |user|
-          UserMailer.send_email(user, params[:subject], params[:message]).deliver_now
-        end
-        redirect_to admin_dashboard_path, notice: 'E-mails enviados com sucesso!'
-      else
-        redirect_to admin_dashboard_path, alert: 'Nenhum destinatário selecionado.'
-      end
+  before_action :authenticate_user!
+  before_action :require_admin
+
+  def create
+    recipients = case params[:recipient_type]
+                when 'all'
+                  User.all
+                when 'active_course'
+                  Course.where(id: params[:course_ids]).map(&:enrollments).flatten.map(&:user)
+                when 'finished_course'
+                  Course.where(id: params[:course_ids]).map(&:enrollments).flatten.map(&:user)
+                else
+                  []
+                end
+    
+    if params[:recipient_type].in?(%w[active_course finished_course]) && params[:course_ids].blank?
+      redirect_to admin_dashboard_path, alert: 'Selecione pelo menos um curso.'
+      return
     end
-  
-    private
-  
-    def require_admin
-      unless current_user.admin?
-        redirect_to root_path, alert: 'Acesso negado.'
+
+    if recipients.any?
+      recipients.uniq.each do |user|
+        UserMailer.send_email(user, params[:subject], params[:message]).deliver_now
       end
+      redirect_to admin_dashboard_path, notice: 'E-mails enviados com sucesso!'
+    else
+      redirect_to admin_dashboard_path, alert: 'Nenhum destinatário selecionado.'
     end
   end
+
+  private
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to root_path, alert: 'Acesso negado.'
+    end
+  end
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,11 @@ class Course < ApplicationRecord
 
     validates :start_date, :end_date, :class_time, :slots, :modality, presence: true
 
+    scope :active, -> { where("end_date >= ?", Time.current) }
+
+    scope :finished, -> { where("end_date < ?", Time.current) }
+
+
     def registration_period_open?
         now = Time.current
         start_date <= now && end_date >= now
